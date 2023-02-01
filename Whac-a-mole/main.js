2024a511e7002fc6cc00a0d362df09a96ac94cb0
jmrepisodev <jmrepisodev@gmail.com>
2023-02-01 13:25:49 +0100

    whac-a-mole

--- Whac-a-mole/main.js
+++ Whac-a-mole/main.js
@@ -4,6 +4,10 @@ const timeLeft=document.querySelector("#time-left");
 const score=document.querySelector("#score");
 
 let result=0;
+let hitPosition;
+let timerID;
+let countDownTimerID;
+let currentTime=10;
 
 function randomSquare(){
 
@@ -12,11 +16,43 @@ function randomSquare(){
         square.classList.remove("mole");
     })
 
-    //selecciona aleatoriamente un elemento del array
+    //selecciona aleatoriamente un elemento del array. De 0 a 8
    let randomSquare=squaresArray[Math.floor(Math.random()*9)];
    console.log(randomSquare);
    //agrega una clase
    randomSquare.classList.add("mole");
+
+   hitPosition=randomSquare.id;
+}
+
+squaresArray.forEach(square=>{
+    square.addEventListener("mousedown", ()=>{
+        if(square.id==hitPosition){
+            result++;
+            console.log(result)
+            score.innerHTML=result;
+            hitPosition=null;
+        }
+    })
+})
+
+function moveMole(){
+    timerID=setInterval(randomSquare,500);
+}
+
+function countDown(){
+    currentTime--;
+    timeLeft.textContent=currentTime;
+
+    if(currentTime===0){
+        clearInterval(countDownTimerID);
+        clearInterval(timerID)
+        alert("GAME OVER. Tu puntuación es :"+result);
+    }
 }
 
 randomSquare()
+
+moveMole()
+
+countDownTimerID=setInterval(countDown,1000);
